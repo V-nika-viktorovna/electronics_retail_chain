04d94a2450814ecc2cb6ae06cf88353afcb47051
Veronika Chernikova <fotonikanvr@yandex.ru>
2025-10-15 18:46:09 +0300

    start

---
+++ electronics_retail_chain/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ElectronicsRetailChainConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'electronics_retail_chain'
